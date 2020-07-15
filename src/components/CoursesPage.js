835291f3a031c8930ef1a7f5a909b299e204e2ae
ma7ammad <kakadumdum@yandex.com>
2020-07-15 18:09:39 +0100

    refactored CoursesPage to use function component instead of class component and implemeted useEffect

--- src/components/CoursesPage.js
+++ src/components/CoursesPage.js
@@ -1,44 +1,40 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { getCourses } from "../api/courseApi";
 
-class CoursesPage extends React.Component {
-  state = {
-    courses: [],
-  };
+function CoursesPage() {
+  const [courses, setCourses] = useState([]);
 
-  //Lifecycle method componentDidMount executes after component is mounted
-  componentDidMount() {
-    getCourses().then((courses) => this.setState({ courses: courses }));
-  }
-
-  render() {
-    return (
-      <>
-        <h2>Courses</h2>
-        <table className="table">
-          <thead>
-            <tr>
-              <th>Title</th>
-              <th>Author ID</th>
-              <th>Category</th>
-            </tr>
-          </thead>
-          <tbody>
-            {this.state.courses.map((course) => {
-              return (
-                // key to resolve Warning: Each child in a list should have a unique "key" prop.
-                <tr key={course.id}>
-                  <th>{course.title}</th>
-                  <th>{course.authorId}</th>
-                  <th>{course.category}</th>
-                </tr>
-              );
-            })}
-          </tbody>
-        </table>
-      </>
-    );
-  }
+  //
+  useEffect(() => {
+    getCourses().then((_courses) => setCourses(_courses));
+  }, []); // declaring an empty dependency array to say this should only run once
+  // otherwise getCourses above will keep calling  setCourses infinitely
+  return (
+    <>
+      <h2>Courses</h2>
+      <table className="table">
+        <thead>
+          <tr>
+            <th>Title</th>
+            <th>Author ID</th>
+            <th>Category</th>
+          </tr>
+        </thead>
+        <tbody>
+          {courses.map((course) => {
+            return (
+              // key to resolve Warning: Each child in a list should have a unique "key" prop.
+              <tr key={course.id}>
+                <th>{course.title}</th>
+                <th>{course.authorId}</th>
+                <th>{course.category}</th>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </>
+  );
 }
 
 export default CoursesPage;

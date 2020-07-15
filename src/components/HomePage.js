d72c5f0d4c63a3ec9786d4c9ca32ce1ce3bec15e
ma7ammad <kakadumdum@yandex.com>
2020-07-15 01:38:28 +0100

    home page + initial index.js

---
+++ src/components/HomePage.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function HomePage() {
+  return (
+    <div className="jumbotron">
+      <h1>Course Administration</h1>
+      <p>React, React Router for ultra-responsive web apps</p>
+    </div>
+  );
+}
+
+export default HomePage;

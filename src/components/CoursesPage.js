f5e5fb2d78408e20f0d0a815ee32e249f568661a
ma7ammad <kakadumdum@yandex.com>
2020-07-15 16:54:46 +0100

    using props, states & lifecycle methods to display a list of courses in the CoursesPage

--- src/components/CoursesPage.js
+++ src/components/CoursesPage.js
@@ -1,8 +1,43 @@
 import React from "react";
+import { getCourses } from "../api/courseApi";
 
 class CoursesPage extends React.Component {
+  state = {
+    courses: [],
+  };
+
+  //Lifecycle method componentDidMount executes after component is mounted
+  componentDidMount() {
+    getCourses().then((courses) => this.setState({ courses: courses }));
+  }
+
   render() {
-    return <h2>Courses</h2>;
+    return (
+      <>
+        <h2>Courses</h2>
+        <table className="table">
+          <thead>
+            <tr>
+              <th>Title</th>
+              <th>Author ID</th>
+              <th>Category</th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.state.courses.map((course) => {
+              return (
+                // key to resolve Warning: Each child in a list should have a unique "key" prop.
+                <tr key={course.id}>
+                  <th>{course.title}</th>
+                  <th>{course.authorId}</th>
+                  <th>{course.category}</th>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      </>
+    );
   }
 }
 

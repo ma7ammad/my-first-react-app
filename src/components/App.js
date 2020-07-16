b75878d6c6706afb3c5d0a166b20e11e04ed8865
ma7ammad <kakadumdum@yandex.com>
2020-07-16 15:58:47 +0100

    added PageNotFound functionality

--- src/components/App.js
+++ src/components/App.js
@@ -3,19 +3,21 @@ import HomePage from "./HomePage";
 import AboutPage from "./AboutPage";
 import CoursesPage from "./CoursesPage";
 import Header from "./common/Header";
-import { Route } from "react-router-dom";
+import { Route, Switch, Redirect } from "react-router-dom";
+import NotFoundPage from "./NotFoundPage";
 
 function App() {
-  /*----------------------------------*/
-  //function declared witin a function
-
-  /*----------------------------------*/
   return (
     <div className="container-fluid">
       <Header />
-      <Route path="/" exact component={HomePage} />
-      <Route path="/courses" component={CoursesPage} />
-      <Route path="/about" component={AboutPage} />
+      {/* Switch statement to exit once a matching path is found, preventing NotFoundPage being displayed on all pages */}
+      <Switch>
+        <Route path="/" exact component={HomePage} />
+        <Route path="/courses" component={CoursesPage} />
+        <Route path="/about" component={AboutPage} />
+        <Redirect from="/about-page" to="about" />
+        <Route component={NotFoundPage} />
+      </Switch>
     </div>
   );
 }

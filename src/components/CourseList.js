bbaae8b50d5e954e13a9d3d699dee2aebe9d8bcb
ma7ammad <kakadumdum@yandex.com>
2020-07-17 02:21:09 +0100

    Added ManageCoursePage with course-slug as content

--- src/components/CourseList.js
+++ src/components/CourseList.js
@@ -1,5 +1,6 @@
 import React from "react";
 import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
 
 function CourseList(props) {
   return (
@@ -16,9 +17,11 @@ function CourseList(props) {
           return (
             // key to resolve Warning: Each child in a list should have a unique "key" prop.
             <tr key={course.id}>
-              <th>{course.title}</th>
-              <th>{course.authorId}</th>
-              <th>{course.category}</th>
+              <td>
+                <Link to={"/course/" + course.slug}>{course.title}</Link>
+              </td>
+              <td>{course.authorId}</td>
+              <td>{course.category}</td>
             </tr>
           );
         })}
